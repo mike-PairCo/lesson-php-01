7a10093aca77b2847ca80f894079144cccab5a19
Jeerasak <jeerasak.t@pair-co.com>
2025-11-28 20:22:23 +0700

    add injection prevent

--- check-login.php
+++ check-login.php
@@ -6,7 +6,12 @@ $password = $_POST["password"];
 
 include "db.php";
 
-$sql = "SELECT * FROM users WHERE username = '$username' AND password = '$password' LIMIT 1";
+$sql = sprintf(
+    "SELECT * FROM users WHERE username = '%s' AND password = '%s' LIMIT 1",
+    mysqli_real_escape_string($db_connection, $username),
+    mysqli_real_escape_string($db_connection, $password)
+);
+
 $result = mysqli_query($db_connection, $sql);
 
 if (mysqli_num_rows($result) > 0) {
@@ -15,8 +20,7 @@ if (mysqli_num_rows($result) > 0) {
     $_SESSION["username"] = $row["username"];
     $_SESSION["user_id"] = $row["id"];
     echo "<script>window.location='dashboard.php'</script>";
-}
-else {
+} else {
     echo "fail";
 }
 
